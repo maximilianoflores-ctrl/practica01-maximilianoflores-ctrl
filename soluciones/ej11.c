d6f247a802785427375b1934a4969bd1903088d3
maximilianoflores-ctrl <matstito7002@gmail.com>
2026-02-21 14:28:20 -0600

    Update ej11.c

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -4,3 +4,10 @@ int main(){
     // Resolver ejercicio 11
     return 0;
 }
+
+
+// &a[0] = 1000
+// &a[1] = 1008
+// &a[2] = 1016
+// 1008 - 1000 = 8 bytes entonces es el tamaño de 8 bytes
+// y posible tipo maybe un double
