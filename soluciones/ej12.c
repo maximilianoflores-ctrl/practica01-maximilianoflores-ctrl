e156c5b1728274794729abc3b83496ba892a8caf
maximilianoflores-ctrl <matstito7002@gmail.com>
2026-02-21 14:31:52 -0600

    Update ej12.c

--- soluciones/ej12.c
+++ soluciones/ej12.c
@@ -4,3 +4,15 @@ int main(){
     // Resolver ejercicio 12
     return 0;
 }
+
+
+// int m[3][5];
+// base = 4000
+// Direccion de m[2][4]
+// 4000 + 14 * 4 = 4056
+
+// Direccion de int m[1][3]
+// 4000 + 8 * 4 = 4032
+
+// Direccion entre ambas
+// 4056 - 4032 = 24 bytes
