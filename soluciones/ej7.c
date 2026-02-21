794f157d7bfcc051372e19f56d31cb2190708615
maximilianoflores-ctrl <matstito7002@gmail.com>
2026-02-21 14:02:45 -0600

    Update ej7.c

--- soluciones/ej7.c
+++ soluciones/ej7.c
@@ -1,7 +1,12 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 7
+   int m[2][4];
+    for(int i = 0; i<2; i++){
+        for(int j = 0; j<4; j++){
+            printf("&m[%d][%d] = %p\n", i, j, (*void)&m[i][j]);
+        }
+    }
     return 0;
 }
 
