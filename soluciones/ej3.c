bce1c88c7713b5833a811742760776f84cddd982
maximilianoflores-ctrl <matstito7002@gmail.com>
2026-02-21 13:36:36 -0600

    Update ej3.c
    se almacenan continuas despume de 3 ejecuciones

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,17 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 3
+    int a[3];
+    for(int i = 0; i<3; i++){
+        printf( "&a[%d] = %p\n", i, (*void)&a[i]);
+    }
+    int b[3];
+    for(int j = 0; j<3; j++){
+        printf("&b[%d] = %p\n, j, (*void)&b[j]);
+            }
+    int c[3];
+        for(int k = 0; k<3; k++){
+            printf("&c[%d] = %p\n, k, (*void)&c[k])
+                }         
     return 0;
 }
