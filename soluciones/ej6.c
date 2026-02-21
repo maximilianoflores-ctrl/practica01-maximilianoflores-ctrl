6faa93029518ec1b87fd785d7230cdaf61edc024
maximilianoflores-ctrl <matstito7002@gmail.com>
2026-02-21 13:56:46 -0600

    Update ej6.c

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,21 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 6
+ int m[2][4];
+    for(int i = 0; i<2; i++){
+        for(int j = 0; j<4; j++){
+            printf("&m[%d][%d] = %p\n", i, j, (*void)&m[i][j]);
+        }
+    }
     return 0;
 }
+
+
+//1. al imprimirlos sale la estructuria m[0][0]
+//                                      m[0][1]
+//                                      m[0][2]
+//                                      m[0][3]
+//                                      m[1][0]
+//                                      m[1][1]
+//2. con el dato del anterior punto puedo decir que el almacenamiento es por filas
+//3. &m[i][j] = base + (i * 4 + j) * sizeof(int)
