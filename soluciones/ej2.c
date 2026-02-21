c9c0589d0e02466002ae1c11132c8b3c77a28809
maximilianoflores-ctrl <matstito7002@gmail.com>
2026-02-21 13:27:27 -0600

    Update ej2.c
    ejercicio2

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -1,6 +1,9 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 2
+  int a[6];
+    for(int i=0; i<6; i++){
+        printf("&a[%d] = %p\n, i, (*void)&a[i];
+            }
     return 0;
 }
