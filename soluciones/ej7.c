e8f307521229cdb2cbbca72c2234a8132abd3f42
maximilianoflores-ctrl <matstito7002@gmail.com>
2026-02-21 14:02:19 -0600

    Update ej7.c

--- soluciones/ej7.c
+++ soluciones/ej7.c
@@ -4,3 +4,16 @@ int main(){
     // Resolver ejercicio 7
     return 0;
 }
+
+
+// este ejercicio nos pide que sin ejecutar justifiquemos cual direccion es mayor, si: &m[0][3] o &m[1][0]
+// la respuesta es &m[1][0]
+//demostracion:
+// usando la formula anterior podriamos decir que:
+//para &m[0][3]:
+//(0 * 4 + 3) = 3
+//base + 3 * 4= base + 12
+//para &m[1][0]:
+//(1 * 4 + 0) = 4
+//base + 4 * 4= base + 16
+// por lo que queda demostrado que &m[1][0] > &m[0][3]
