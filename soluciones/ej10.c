cb8f4e5bcc5236982ebb6ef41e31635d28bc5620
maximilianoflores-ctrl <matstito7002@gmail.com>
2026-02-21 14:26:01 -0600

    Update ej10.c

--- soluciones/ej10.c
+++ soluciones/ej10.c
@@ -1,6 +1,14 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 10
+    int gigante[10000000]
+
+    int main() {
+        gigante [0] = 123
+    gigante[999999] = 456
+    printf("Okp\n");
     return 0;
 }
+
+//no supe bien como declarar este mismo je
+//suponiendo que fallo no sabrai explicar la causa y tampoco las demas preguntas
