e91e162fc546fcb25be51c4927432f99261d9533
maximilianoflores-ctrl <matstito7002@gmail.com>
2026-02-21 14:13:51 -0600

    Update ej8.c

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -4,3 +4,16 @@ int main(){
     // Resolver ejercicio 8
     return 0;
 }
+
+// el problema nos da estos datos: base=2000, sizeof(int)=4, m[1][2]= 2024
+//nos piden el numero de columnas y el tamaño total
+//Para el numero de columnas:
+// 2024 = 2000 + (1 + columnas + 2) * 4
+// 2024 - 2000 = 24
+// (1 * columnas + 2) *4 = 24
+// 1 * columnas + 2 = 6
+// columnas = 4
+// Para el tamaño total:
+// int m[2][4]
+// 2*4= 8
+// 8*4 = 32 bytes
